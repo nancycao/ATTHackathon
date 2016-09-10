64fc8c19ae2038e1f4945db3fd6bef8c0c50febe
nancycao <ncao@stuy.edu>
2016-09-10 16:41:42 -0400

    Adding pages

---
+++ backend.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+print ("Content-Type:text/html\n")
+print ("")
+
+# <name>
+# IntroCS2 pd<#>
+# HW #<n> -- <title>
+# 2015-<m>-<d>
+
+heading='''
+<!DOCTYPE html>
+<html>
+'''
+
+code='''
+====CODE====
+'''
+
+closing='''
+</html>
+'''
+
+print (heading+code+closing)
